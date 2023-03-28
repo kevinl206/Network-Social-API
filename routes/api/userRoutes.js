5b526c801c0068d69382d501cb21cb6bb2ebbaea
Kevin Lang <kevinl20@yahoo.com>
2023-03-27 21:43:42 -0700

    api routes for user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,23 @@
-const { Schema, model } = require('mongoose');
+const router = require('express').Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
 
-const studentSchema = new Schema(
+// /api/users
+router.route('/').get(getUser).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
